214548e7aadae57229a4c78209b80f7ad0f9d980
lxer19 <lxerleap@gmail.com>
2023-03-20 21:18:15 -0700

    add SGCN

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from dgl.nn import GraphConv, SAGEConv, APPNPConv, GATConv
+from dgl.nn import GraphConv, SAGEConv, APPNPConv, GATConv,SGConv
 
 
 class MLP(nn.Module):
@@ -198,7 +198,6 @@ class GCN(nn.Module):
                 h = self.dropout(h)
         return h_list, h
 
-
 class GAT(nn.Module):
     def __init__(
         self,
@@ -343,6 +342,328 @@ class APPNP(nn.Module):
         h = self.propagate(g, h)
         return h_list, h
 
+    
+class SGCN(nn.Module):
+    def __init__(
+        self,
+        num_layers,
+        input_dim,
+        hidden_dim,
+        output_dim,
+        dropout_ratio,
+        activation,
+        norm_type="none",
+    ):
+        super().__init__()
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.norm_type = norm_type
+        self.activation = activation
+        self.dropout = nn.Dropout(dropout_ratio)
+        self.layers = nn.ModuleList()
+        self.norms = nn.ModuleList()
+
+        if num_layers == 1:
+            self.layers.append(SGConv(input_dim, output_dim, "gcn"))
+        else:
+            self.layers.append(SGConv(input_dim, hidden_dim, "gcn"))
+            if self.norm_type == "batch":
+                self.norms.append(nn.BatchNorm1d(hidden_dim))
+            elif self.norm_type == "layer":
+                self.norms.append(nn.LayerNorm(hidden_dim))
+
+            for i in range(num_layers - 2):
+                self.layers.append(SGConv(hidden_dim, hidden_dim, "gcn"))
+                if self.norm_type == "batch":
+                    self.norms.append(nn.BatchNorm1d(hidden_dim))
+                elif self.norm_type == "layer":
+                    self.norms.append(nn.LayerNorm(hidden_dim))
+
+            self.layers.append(SGConv(hidden_dim, output_dim, "gcn"))
+
+    def forward(self, blocks, feats):
+        h = feats
+        h_list = []
+        for l, (layer, block) in enumerate(zip(self.layers, blocks)):
+            # We need to first copy the representation of nodes on the RHS from the
+            # appropriate nodes on the LHS.
+            # Note that the shape of h is (num_nodes_LHS, D) and the shape of h_dst
+            # would be (num_nodes_RHS, D)
+            h_dst = h[: block.num_dst_nodes()]
+            # Then we compute the updated representation on the RHS.
+            # The shape of h now becomes (num_nodes_RHS, D)
+            h = layer(block, (h, h_dst))
+            if l != self.num_layers - 1:
+                h_list.append(h)
+                if self.norm_type != "none":
+                    h = self.norms[l](h)
+                h = self.activation(h)
+                h = self.dropout(h)
+        return h_list, h
+
+    def inference(self, dataloader, feats):
+        """
+        Inference with the GraphSAGE model on full neighbors (i.e. without neighbor sampling).
+        dataloader : The entire graph loaded in blocks with full neighbors for each node.
+        feats : The input feats of entire node set.
+        """
+        device = feats.device
+        for l, layer in enumerate(self.layers):
+            y = torch.zeros(
+                feats.shape[0],
+                self.hidden_dim if l != self.num_layers - 1 else self.output_dim,
+            ).to(device)
+            for input_nodes, output_nodes, blocks in dataloader:
+                block = blocks[0].int().to(device)
+
+                h = feats[input_nodes]
+                h_dst = h[: block.num_dst_nodes()]
+                h = layer(block, (h, h_dst))
+                if l != self.num_layers - 1:
+                    if self.norm_type != "none":
+                        h = self.norms[l](h)
+                    h = self.activation(h)
+                    h = self.dropout(h)
+
+                y[output_nodes] = h
+
+            feats = y
+        return y    
+    
+    
+    
+
+class GraphSAINT(nn.Module):
+    def __init__(self, arch_gcn, train_params, feat_full, label_full, cpu_eval=False):
+        """
+        Build the multi-layer GNN architecture.
+        Inputs:
+            num_classes         int, number of classes a node can belong to
+            arch_gcn            dict, config for each GNN layer
+            train_params        dict, training hyperparameters (e.g., learning rate)
+            feat_full           np array of shape N x f, where N is the total num of
+                                nodes and f is the dimension for input node feature
+            label_full          np array, for single-class classification, the shape
+                                is N x 1 and for multi-class classification, the
+                                shape is N x c (where c = num_classes)
+            cpu_eval            bool, if True, will put the model on CPU.
+        Outputs:
+            None
+        """
+        super(GraphSAINT,self).__init__()
+        self.use_cuda = (args_global.gpu >= 0)
+        if cpu_eval:
+            self.use_cuda=False
+        if "attention" in arch_gcn:
+            if "gated_attention" in arch_gcn:
+                if arch_gcn['gated_attention']:
+                    self.aggregator_cls = layers.GatedAttentionAggregator
+                    self.mulhead = int(arch_gcn['attention'])
+            else:
+                self.aggregator_cls = layers.AttentionAggregator
+                self.mulhead = int(arch_gcn['attention'])
+        else:
+            self.aggregator_cls = layers.HighOrderAggregator
+            self.mulhead = 1
+        self.weight_decay = train_params['weight_decay']
+        self.dropout = train_params['dropout']
+        self.lr = train_params['lr']
+        self.arch_gcn = arch_gcn
+        self.sigmoid_loss = (arch_gcn['loss'] == 'sigmoid')
+        self.feat_full = torch.from_numpy(feat_full.astype(np.float32))
+        self.label_full = torch.from_numpy(label_full.astype(np.float32))
+        if self.use_cuda:
+            self.feat_full = self.feat_full.cuda()
+            self.label_full = self.label_full.cuda()
+        if not self.sigmoid_loss:
+            self.label_full_cat = torch.from_numpy(label_full.argmax(axis=1).astype(np.int64))
+            if self.use_cuda:
+                self.label_full_cat = self.label_full_cat.cuda()
+        self.num_classes = num_classes
+        _dims, self.order_layer, self.act_layer, self.bias_layer, self.aggr_layer \
+                        = parse_layer_yml(arch_gcn, self.feat_full.shape[1])
+        # get layer index for each conv layer, useful for jk net last layer aggregation
+        self.set_idx_conv()
+        self.set_dims(_dims)
+
+        self.loss = 0
+        self.opt_op = None
+
+        # build the model below
+        self.num_params = 0
+        self.aggregators, num_param = self.get_aggregators()
+        self.num_params += num_param
+        self.conv_layers = nn.Sequential(*self.aggregators)
+        self.num_params += self.classifier.num_param
+
+    def set_dims(self, dims):
+        """
+        Set the feature dimension / weight dimension for each GNN or MLP layer.
+        We will use the dimensions set here to initialize PyTorch layers.
+        Inputs:
+            dims        list, length of node feature for each hidden layer
+        Outputs:
+            None
+        """
+        self.dims_feat = [dims[0]] + [
+            ((self.aggr_layer[l]=='concat') * self.order_layer[l] + 1) * dims[l+1]
+            for l in range(len(dims) - 1)
+        ]
+        self.dims_weight = [(self.dims_feat[l],dims[l+1]) for l in range(len(dims)-1)]
+
+    def set_idx_conv(self):
+        """
+        Set the index of GNN layers for the full neural net. For example, if
+        the full NN is having 1-0-1-0 arch (1-hop graph conv, followed by 0-hop
+        MLP, ...). Then the layer indices will be 0, 2.
+        """
+        idx_conv = np.where(np.array(self.order_layer) >= 1)[0]
+        idx_conv = list(idx_conv[1:] - 1)
+        idx_conv.append(len(self.order_layer) - 1)
+        _o_arr = np.array(self.order_layer)[idx_conv]
+        if np.prod(np.ediff1d(_o_arr)) == 0:
+            self.idx_conv = idx_conv
+        else:
+            self.idx_conv = list(np.where(np.array(self.order_layer) == 1)[0])
+
+
+    def forward(self, node_subgraph, adj_subgraph):
+        feat_subg = self.feat_full[node_subgraph]
+        label_subg = self.label_full[node_subgraph]
+        label_subg_converted = label_subg if self.sigmoid_loss else self.label_full_cat[node_subgraph]
+        _, emb_subg = self.conv_layers((adj_subgraph, feat_subg))
+        emb_subg_norm = F.normalize(emb_subg, p=2, dim=1)
+        pred_subg = self.classifier((None, emb_subg_norm))[1]
+        return pred_subg, label_subg, label_subg_converted
+
+
+    def get_aggregators(self):
+        """
+        Return a list of aggregator instances. to be used in self.build()
+        """
+        num_param = 0
+        aggregators = []
+        for l in range(self.num_layers):
+            aggr = self.aggregator_cls(
+                    *self.dims_weight[l],
+                    dropout=self.dropout,
+                    act=self.act_layer[l],
+                    order=self.order_layer[l],
+                    aggr=self.aggr_layer[l],
+                    bias=self.bias_layer[l],
+                    mulhead=self.mulhead,
+            )
+            num_param += aggr.num_param
+            aggregators.append(aggr)
+        return aggregators, num_param
+
+    def predict(self, preds):
+        return nn.Sigmoid()(preds) if self.sigmoid_loss else F.softmax(preds, dim=1)
+
+class HighOrderAggregator(nn.Module):
+    def __init__(self, dim_in, dim_out, dropout=0., act='relu', \
+            order=1, aggr='mean', bias='norm-nn', **kwargs):
+        """
+        Layer implemented here combines the GraphSAGE-mean [1] layer with MixHop [2] layer.
+        We define the concept of `order`: an order-k layer aggregates neighbor information
+        from 0-hop all the way to k-hop. The operation is approximately:
+            X W_0 [+] A X W_1 [+] ... [+] A^k X W_k
+        where [+] is some aggregation operation such as addition or concatenation.
+        Special cases:
+            Order = 0  -->  standard MLP layer
+            Order = 1  -->  standard GraphSAGE layer
+        [1]: https://cs.stanford.edu/people/jure/pubs/graphsage-nips17.pdf
+        [2]: https://arxiv.org/abs/1905.00067
+        Inputs:
+            dim_in      int, feature dimension for input nodes
+            dim_out     int, feature dimension for output nodes
+            dropout     float, dropout on weight matrices W_0 to W_k
+            act         str, activation function. See F_ACT at the top of this file
+            order       int, see definition above
+            aggr        str, if 'mean' then [+] operation adds features of various hops
+                            if 'concat' then [+] concatenates features of various hops
+            bias        str, if 'bias' then apply a bias vector to features of each hop
+                            if 'norm' then perform batch-normalization on output features
+        Outputs:
+            None
+        """
+        super(HighOrderAggregator,self).__init__()
+        assert bias in ['bias', 'norm', 'norm-nn']
+        F_ACT = {'relu': nn.ReLU(),
+         'I': lambda x:x}
+        self.order, self.aggr = order, aggr
+        self.act, self.bias = F_ACT[act], bias
+        self.dropout = dropout
+        self.f_lin, self.f_bias = [], []
+        self.offset, self.scale = [], []
+        self.num_param = 0
+        for o in range(self.order + 1):
+            self.f_lin.append(nn.Linear(dim_in, dim_out, bias=False))
+            nn.init.xavier_uniform_(self.f_lin[-1].weight)
+            self.f_bias.append(nn.Parameter(torch.zeros(dim_out)))
+            self.num_param += dim_in * dim_out
+            self.num_param += dim_out
+            self.offset.append(nn.Parameter(torch.zeros(dim_out)))
+            self.scale.append(nn.Parameter(torch.ones(dim_out)))
+            if self.bias == 'norm' or self.bias == 'norm-nn':
+                self.num_param += 2 * dim_out
+        self.f_lin = nn.ModuleList(self.f_lin)
+        self.f_dropout = nn.Dropout(p=self.dropout)
+        self.params = nn.ParameterList(self.f_bias + self.offset + self.scale)
+        self.f_bias = self.params[:self.order + 1]
+        if self.bias == 'norm':
+            self.offset = self.params[self.order + 1 : 2 * self.order + 2]
+            self.scale = self.params[2 * self.order + 2 : ]
+        elif self.bias == 'norm-nn':
+            final_dim_out = dim_out * ((aggr=='concat') * (order + 1) + (aggr=='mean'))
+            self.f_norm = nn.BatchNorm1d(final_dim_out, eps=1e-9, track_running_stats=True)
+        self.num_param = int(self.num_param)
+
+    def _spmm(self, adj_norm, _feat):
+        """ sparce feature matrix multiply dense feature matrix """
+        # alternative ways: use geometric.propagate or torch.mm
+        return torch.sparse.mm(adj_norm, _feat)
+
+    def _f_feat_trans(self, _feat, _id):
+        feat = self.act(self.f_lin[_id](_feat) + self.f_bias[_id])
+        if self.bias == 'norm':
+            mean = feat.mean(dim=1).view(feat.shape[0],1)
+            var = feat.var(dim=1, unbiased=False).view(feat.shape[0], 1) + 1e-9
+            feat_out = (feat - mean) * self.scale[_id] * torch.rsqrt(var) + self.offset[_id]
+        else:
+            feat_out = feat
+        return feat_out
+
+    def forward(self, blocks, feats):
+        """
+        Inputs:.
+            adj_norm        normalized adj matrix of the subgraph
+            feat_in         2D matrix of input node features
+        Outputs:
+            adj_norm        same as input (to facilitate nn.Sequential)
+            feat_out        2D matrix of output node features
+        """
+        adj_norm, feat_in = blocks, feats
+        print(blocks,feats)
+        feat_in = self.f_dropout(feat_in)
+        feat_hop = [feat_in]
+        # generate A^i X
+        for o in range(self.order):
+            # propagate(edge_index, x=x, norm=norm)
+            feat_hop.append(self._spmm(adj_norm, feat_hop[-1]))
+        feat_partial = [self._f_feat_trans(ft, idf) for idf, ft in enumerate(feat_hop)]
+        if self.aggr == 'mean':
+            feat_out = feat_partial[0]
+            for o in range(len(feat_partial) - 1):
+                feat_out += feat_partial[o + 1]
+        elif self.aggr == 'concat':
+            feat_out = torch.cat(feat_partial, 1)
+        else:
+            raise NotImplementedError
+        if self.bias == 'norm-nn':
+            feat_out = self.f_norm(feat_out)
+        return adj_norm, feat_out       # return adj_norm to support Sequential
 
 class Model(nn.Module):
     """
@@ -401,6 +722,27 @@ class Model(nn.Module):
                 activation=F.relu,
                 norm_type=conf["norm_type"],
             ).to(conf["device"])
+        elif "SGCN" in conf["model_name"]:
+            self.encoder = GCN(
+                num_layers=conf["num_layers"],
+                input_dim=conf["feat_dim"],
+                hidden_dim=conf["hidden_dim"],
+                output_dim=conf["label_dim"],
+                dropout_ratio=conf["dropout_ratio"],
+                activation=F.relu,
+                norm_type=conf["norm_type"],
+            ).to(conf["device"])
+        elif "GraphSAINT" in conf["model_name"]:
+            self.encoder = GraphSAINT(
+                in_dim=conf["feat_dim"],
+                hid_dim=conf["hidden_dim"],
+                out_dim=conf["label_dim"],
+            ).to(conf["device"])
+        elif "mix_hop_sage" in conf["model_name"]:
+            self.encoder = HighOrderAggregator(
+               dim_in=conf["feat_dim"],
+               dim_out=conf["label_dim"],
+            ).to(conf["device"])
 
     def forward(self, data, feats):
         """
@@ -427,3 +769,4 @@ class Model(nn.Module):
             return self.encoder.inference(data, feats)
         else:
             return self.forward(data, feats)
+        
